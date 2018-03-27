9559277a9e491de818aeb9d072e9562b3e792cdb
pavan muppa <pawankumar.muppa@gmail.com>
2018-03-27 12:43:43 +0530

    New changes done to get title program

--- src/basics/GetTitle_of_page.java
+++ src/basics/GetTitle_of_page.java
@@ -5,10 +5,7 @@ import java.util.concurrent.TimeUnit;
 import org.openqa.selenium.By;
 import org.openqa.selenium.Keys;
 import org.openqa.selenium.WebDriver;
-import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
-import org.openqa.selenium.support.ui.ExpectedConditions;
-import org.openqa.selenium.support.ui.WebDriverWait;
 import org.testng.annotations.Test;
 
 public class GetTitle_of_page {
